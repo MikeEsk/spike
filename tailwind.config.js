d2edb577d6e088b6eaa68395dee29ddff3e22060
Mike <55053694+MikeEsk@users.noreply.github.com>
2024-04-28 11:58:13 -0500

    override global ignore

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        rotate: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+        reverseRotate: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(-360deg)' },
+        },
+        expRotate: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(36000deg)' }, // Rotate 100 times
+        },
+        slideInLeft: {
+          'from': { transform: 'translateX(-100%)' },
+          'to': { transform: 'translateX(0)' },
+        },
+        slideInRight: {
+          'from': { transform: 'translateX(100%)' },
+          'to': { transform: 'translateX(0)' },
+        }
+      },
+      animation: {
+        'cw-spin': 'rotate 180s linear infinite',
+        'ccw-spin': 'reverseRotate 180s linear infinite',
+        'exp-spin': 'expRotate 6s cubic-bezier(0.1, 0.9, 0.3, 1) reverse',
+        'slideInLeft': 'slideInLeft 0.5s forwards ease-out',
+        'slideInRight': 'slideInRight 0.5s forwards ease-out'
+      }
+    }
+  },
+  plugins: [],
+}
